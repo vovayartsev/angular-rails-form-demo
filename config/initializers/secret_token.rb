768bad85b26c3710c3b24f61f6f8f70743f321f2
Vladimir Yartsev <vovayartsev@gmail.com>
2014-01-25 21:12:11 +0400

    Scaffolding and gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularRailsFormDemo::Application.config.secret_key_base = 'f7bdf1bcae86bdd2190f5f45760716a7f430671420428b10cbc1af3add83ced749d42b1df992ce049bc4a5be1845111f0c276fa4cf4efdeeadd935003b5ac5bf'
