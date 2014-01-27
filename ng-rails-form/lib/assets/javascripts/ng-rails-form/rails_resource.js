5e61e0c23269ddcadf0813d714fb52eb7e0b64a9
Vladimir Yartsev <vovayartsev@gmail.com>
2014-01-27 16:49:50 +0400

    refactoring in RailsResource

--- ng-rails-form/lib/assets/javascripts/ng-rails-form/rails_resource.js
+++ ng-rails-form/lib/assets/javascripts/ng-rails-form/rails_resource.js
@@ -5,33 +5,30 @@ angular.module("ngRailsForm").factory("RailsResource", ["$resource", function ($
         var requestRoot = params.requestRoot;
         delete(params.requestRoot);
 
-        // example of conversion:
-        // from: {name: "Joe", email: "joe@example.com"}
-        // to: {user: {name: "Joe", email: "joe@example.com"}}
-        var addRootFn = function(data){
-            var result;
-            if (angular.isObject(data)) {
-                if (requestRoot) {
-                    result = {};
-                    result[requestRoot] = data;
-                } else {
-                    result = data;
-                }
-                result = angular.toJson(result);
-            } else {
-                result = data;
-            }
-            return result;
+        var defaults = {
+            update: {method: "put", isArray: false },
+            create: {method: "post" }
         };
 
-        var defaults = {
-            update: {method: "put", isArray: false}, //, transformRequest: addRootFn },
-            create: {method: "post", transformRequest: addRootFn }
-        };
+        if (requestRoot) {
+            // adding a transformer fn which wraps data with a new root. Example:
+            // from: {name: "Joe", email: "joe@example.com"}
+            // to: {user: {name: "Joe", email: "joe@example.com"}}
+            defaults.update.transformRequest = defaults.create.transformRequest = function(data) {
+                if (angular.isObject(data)) {
+                    var result = {};
+                    result[requestRoot] = data;
+                    return angular.toJson(result);
+                } else {
+                    return  data;
+                }
+            }
+        }
 
         var actions = angular.extend(defaults, userProvidedActions);
 
         var resource = $resource(url, params, actions);
+
         resource.prototype.$save = function () {
             var fn = (this.id) ? this.$update : this.$create;
             return fn.apply(this, arguments);
